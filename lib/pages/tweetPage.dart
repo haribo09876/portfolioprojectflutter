1f231410da38049e634a658d56ea68cddb28f1f5
haribo09876 <haribo09876@gmail.com>
2024-07-31 20:01:48 +0900

    20240731
    by KYW

--- lib/pages/tweetPage.dart
+++ lib/pages/tweetPage.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
 class TweetPage extends StatelessWidget {
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Dialog Title'),
+          content: Text('This is a simple dialog message.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Here is TweetPage'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tweet Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Here is TweetPage'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showMyDialog(context);
+              },
+              child: Text('Show Dialog'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
