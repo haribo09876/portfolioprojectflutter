dc1d87afca7eb09618f7da36cd0a5e77d02fc78f
haribo09876 <haribo09876@gmail.com>
2024-07-31 15:10:23 +0900

    20240731
    by KYW

--- android/app/src/main/kotlin/com/example/portfolioprojectflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/portfolioprojectflutter/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.portfolioprojectflutter
 
+import android.content.Intent
+import id.laskarmedia.openvpn_flutter.OpenVPNFlutterPlugin
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        OpenVPNFlutterPlugin.connectWhileGranted(requestCode == 24 && resultCode == RESULT_OK)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
