27cdf57b2a755af6aec16ded7142ea331c679414
haribo09876 <haribo09876@gmail.com>
2024-08-08 19:19:50 +0900

    20240808
    by KYW

--- lib/pages/shopPage.dart
+++ lib/pages/shopPage.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/login.dart';
 
 class ShopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Here is ShopPage'),
+    final loginService = Provider.of<LoginService>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop'),
+      ),
+      body: Center(
+        child: loginService.isLoggedIn
+            ? Text('환영합니다, ${loginService.userEmail}님!',
+                style: TextStyle(fontSize: 24))
+            : Text('로그인 후에 쇼핑할 수 있습니다.', style: TextStyle(fontSize: 24)),
+      ),
     );
   }
 }
--- lib/services/login.dart
+++ lib/services/login.dart
@@ -5,8 +5,10 @@ import 'dart:convert';
 
 class LoginService extends ChangeNotifier {
   bool _isLoggedIn = false;
+  String? _userEmail;
 
   bool get isLoggedIn => _isLoggedIn;
+  String? get userEmail => _userEmail;
 
   Future<Map<String, dynamic>> loginUser(String email, String password) async {
     final url = dotenv.env['USER_FUNC_URL'];
@@ -20,6 +22,7 @@ class LoginService extends ChangeNotifier {
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = jsonDecode(response.body);
       _isLoggedIn = true;
+      _userEmail = data['user']['userEmail'];
       return {'success': true, 'data': data};
     } else {
       final Map<String, dynamic> errorData = jsonDecode(response.body);
@@ -29,6 +32,7 @@ class LoginService extends ChangeNotifier {
 
   Future<void> logoutUser() async {
     _isLoggedIn = false;
+    _userEmail = null;
     notifyListeners();
   }
 }
